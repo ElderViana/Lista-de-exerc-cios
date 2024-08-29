277f94ce3bb1262b3f69b3008b3fae0c65b44bc2
Elder Viana <hedervianadasilva@gmail.com>
2024-08-29 13:10:09 -0300

    o

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+const exerciseList = [
+    {group: 'Peito', name: 'Supino reto com barra', series: '3 ou 4', repetitions: '8 a 15' },
+    {group: 'Peito', name: 'Supino reto com halteres', series: '3 ou 4', repetitions: '8 a 15' },
+    {group: 'Peito', name: 'Supino inclinado com barra', series: '3 ou 4', repetitions: '8 a 15' },
+    {group: 'Peito', name: 'Supino inclinado com halteres', series: '3 ou 4', repetitions: '8 a 15' },
+    {group: 'Peito', name: 'Supino declinado com barra', series: '3 ou 4', repetitions: '8 a 15' },
+    {group: 'Peito', name: 'Supino declinado com halteres', series: '3 ou 4', repetitions: '8 a 15' },
+
+];
