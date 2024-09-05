d65b05e305fd56b587a03011f48e97c0460653da
Elder Viana <hedervianadasilva@gmail.com>
2024-09-05 12:15:51 -0300

    'y'

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,4 @@
+
 const exerciseList = [
     {group: 'Peito', name: 'Supino reto com barra', series: '3 ou 4', repetitions: '8 a 15'},
     {group: 'Peito', name: 'Supino reto com halteres', series: '3 ou 4', repetitions: '8 a 15'},
@@ -5,28 +6,54 @@ const exerciseList = [
     {group: 'Peito', name: 'Supino inclinado com halteres', series: '3 ou 4', repetitions: '8 a 15'},
     {group: 'Peito', name: 'Supino declinado com barra', series: '3 ou 4', repetitions: '8 a 15'},
     {group: 'Peito', name: 'Supino declinado com halteres', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Peito', name: 'Crucifixo reto com halteres', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Peito', name: 'Cucifixo inclinado com halteres', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Peito', name: 'Crucifixo declinado com halteres', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Peito', name: 'Flexão de braço no solo', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Peito', name: 'Cruzamento de cabo', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Peito', name: 'Mergulho', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Costas', name: 'Puxada frontal', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Costas', name: 'Remada cavalinho', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Costas', name: 'Remada unilateral', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Costas', name: 'Pullover polia', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Costas', name: 'Fly inverso', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Costas', name: 'Barra fixa', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Costas', name: 'Remada baixa', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Costas', name: 'Extensão lombar na máquina', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Bíceps', name: 'Rosca direta com barra reta', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Bíceps', name: 'Rosca direta com barra EZ', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Bíceps', name: 'Rosca direta com halteres', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Bíceps', name: 'Rosca direta alternada com halteres', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Bíceps', name: 'Rosca concentrada com halteres', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Bíceps', name: 'Rosca Scott com barra EZ', series: '3 ou 4', repetitions: '8 a 15'},
     {group: 'Tríceps', name: 'Tríceps testa', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Tríceps', name: 'Tríceps corda', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Tríceps', name: 'Mergulho em barras paralelas', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Tríceps', name: 'Pulley', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Tríceps', name: 'Tríceps coice', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Tríceps', name: 'Tríceps francês', series: '3 ou 4', repetitions: '8 a 15'},
     {group: 'Tríceps', name: 'Mergulho no banco', series: '3 ou 4', repetitions: '8 a 15'},
-    {group: 'Tríceps', name: 'Mergulho nas paralelas', series: '3 ou 4', repetitions: '8 a 15'},
-   
+    {group: 'Tríceps', name: 'Mergulho no banco', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Antebraço', name: 'Rosca de punho', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Antebraço', name: 'Rosca de punho ivertido', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Antebraço', name: 'Rosca de punho ivertido com halteres', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Antebraço', name: 'Rosca de punho com halteres', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Ombro', name: 'Elevação lateral', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Ombro', name: 'Elevação frontal', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Ombro', name: 'Desenvolvimento com halteres', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Ombro', name: 'Desenvolvimento na máquina', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Ombro', name: 'Elevação lateral no cross', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Ombro', name: 'Desenvolvimento frontal com barra', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Trapézio', name: 'Encolhimento de ombro com barra', series: '3 ou 4', repetitions: '8 a 15'}
+    {group: 'Trapézio', name: 'Encolhimento de ombro com halteres', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Leg press', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Agachamento barra solta', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Agachamento sumô', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Agachamento búlgaro', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Cadeira extensora', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Cadeira flexora', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Mesa flexora', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Cadeira abdutora', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Elevação de quadril', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Stiff', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Elevação de panturrilha em pé', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Agachamento livre', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Elevação de panturrilha em pé no smith', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Perna', name: 'Afundo com halteres', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Abdominal', name: 'Elevação de pernas', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Abdominal', name: 'Prancha', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Abdominal', name: 'Prancha lateral', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Abdominal', name: 'Prancha lateral', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Abdominal', name: 'Elevação de quadril', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Abdominal', name: 'Abdominal bicicleta', series: '3 ou 4', repetitions: '8 a 15'},
+    {group: 'Abdominal', name: 'Rotação russa', series: '3 ou 4', repetitions: '8 a 15'},
+
 ];
+
+const createList = () => {
+    let muscleGroup = document.createElement('peito');
+    muscleGroup.p = 'Peito';
+
+}
+createList()
+prompt()
