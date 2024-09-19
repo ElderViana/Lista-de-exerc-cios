5dfbabf32978ba94d796ec5c891ca560acbeffde
Elder Viana <hedervianadasilva@gmail.com>
2024-09-19 11:50:51 -0300

    'o'

--- Js/script.js
+++ Js/script.js
@@ -138,13 +138,22 @@ const createTable = () => {
     console.log(groupList)
     groupList.push(selectMuscleGroup.value);
     for( u = 0; u < tableList.length; u++){
-        
-        if(selectModel.value == 'ABC'){
+        if( selectWeekdays.value == '5'){
+
             console.log('oi')
+
+        }
+        if( selectWeekdays.value == '6'){
+
+            console.log('hello')
+        }
+
+        if(selectModel.value == 'ABC'){
+            
         }
 
         if(selectModel.value == 'ABCDE'){
-            console.log('hello')
+          
         }
         const chosenList = JSON.parse(localStorage.getItem('chosenList'))|| [];
         tableList[u].innerHTML = 
